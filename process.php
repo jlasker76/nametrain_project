d2e1960529134793d555600fa75f685188874439
JONATHAN LASKER <jonathanlasker@MacBook-Pro.local>
2021-03-23 23:36:46 -0600

    updated to actually save

--- process.php
+++ process.php
@@ -1,6 +1,35 @@
 <?php include("db.php");
 
+//echo 'post names= '. $_POST['names'];
+foreach ($_POST['names'] as $value) {
+  $x = 0;
+  $names_str.= implode(",",$value).", "; 
+  /*
+  while($x <= count($value)) {
+    //echo "The number is: $value[$x] <br>";
+    echo "implode= ".implode($value);
+    $x++;
+  }
+  */ 
+  //echo "$value <br>";
+}
 
-$names = sanitize($_POST['names']);
-echo $names;
+echo 'will save='. $names_str;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO  nametrain (names) VALUES('".$names_str."')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
